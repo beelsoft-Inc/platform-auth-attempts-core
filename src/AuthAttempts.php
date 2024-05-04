d3031f31865dd5cdd70589fa9e836eb521e27cc5
Mr Kun <tungnt@ommani.vn>
2024-05-04 16:43:24 +0700

    git init

---
+++ src/AuthAttempts.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tungnt\AuthAttempts;
+
+use Tungnt\Admin\Extension;
+
+class AuthAttempts extends Extension
+{
+    public $name = 'auth-attempts';
+
+    public $views = __DIR__ . '/../resources/views';
+}
